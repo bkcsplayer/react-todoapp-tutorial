9084873654f85b888903ced8a8e4bee4ab13c689
Elias <e.wambugu192@gmail.com>
2021-11-19 11:00:35 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react';
+
+const config = {initialColorMode: 'dark'}
+const theme = extendTheme({config})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
